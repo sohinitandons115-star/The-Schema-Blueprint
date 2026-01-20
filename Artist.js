c5360785ed2b3fdd30c47588894186f83f471839
sohinitandons115-star <sohini.tandon.s.115@kalvium.community>
2026-01-20 15:40:22 +0530

    Delete Artist.js

--- Artist.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const artistSchema = new mongoose.Schema({
-  name: String,
-  genre: String,
-  bio: String
-});
-
-export default mongoose.model("Artist", artistSchema);
